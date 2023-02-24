32d864e0bd5e466e9e132bdc72b11e4b12e092f5
Fauziditya <fauziaditya19082005@gmail.com>
2023-02-24 21:06:37 +0700

    add view admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('template/sidebar');
+        $this->load->view('template/navbar');
+        $this->load->view('admin/index');
+        $this->load->view('template/footer');
+    }
+}
